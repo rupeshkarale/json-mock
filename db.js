58cfa9a55aa3d94e6adf65a084629054f47df7cb
Rupesh Karale <rupeshkarale3245@gmail.com>
2022-12-10 12:36:48 +0530

    products data added

---
+++ db.js
@@ -0,0 +1,12 @@
+const dermproducts = require("./data/dermproducts");
+
+
+
+
+module.exports = {
+  dermproducts
+ 
+}
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
